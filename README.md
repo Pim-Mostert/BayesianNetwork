71d18f49062065b5f34f5a03bd0527d8309fdd57
Pim Mostert <mostert.pim@gmail.com>
2022-06-09 14:35:51 +0200

    wip

---
+++ README.md
@@ -0,0 +1,8 @@
+One of my hobby projects, intended for learning and trying out.
+
+I use:
+- Spyder for Python code
+- JupyterLabApp for notebooks
+- Visual Studio Code for documentation
+
+The repository contains Visual Studio Code settings and recommend extensions in order to render the diagrams and formulas locally.
--- bayesian_network/common/statistics.py
+++ bayesian_network/common/statistics.py
@@ -3,8 +3,8 @@ from typing import Tuple, Iterable, Union
 import torch
 
 
-def generate_random_probability_matrix(size: Union[int, Iterable[int]]):
-    p: torch.tensor = torch.rand(size, dtype=torch.double)
+def generate_random_probability_matrix(size: Union[int, Iterable[int]], device: torch.device):
+    p: torch.tensor = torch.rand(size, dtype=torch.double, device=device)
 
     return p / p.sum(dim=-1, keepdim=True)
 
