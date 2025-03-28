dd412397d1f6ea0599523b4c6230ca2184c11ab7
Pim Mostert <mostert.pim@gmail.com>
2025-03-28 14:37:44 +0100

    Fix node sampler test

--- tests/samplers/test_torch_nodesamplers.py
+++ tests/samplers/test_torch_nodesamplers.py
@@ -10,70 +10,42 @@ from bayesian_network.common.torch_settings import TorchSettings
 from bayesian_network.samplers.torch_sampler import NodeSampler
 
 
-class TorchNodeSamplerTestBase:
-    class TestTorchNodeSampler(TestCase):
-        @abstractmethod
-        def get_torch_settings(self) -> TorchSettings:
-            pass
-
-        def setUp(self):
-            self.num_samples = 10000
-            self.alpha = 0.001
-
-        def test_node_sampler(self):
-            device = self.get_torch_settings().device
-            dtype = self.get_torch_settings().dtype
-
-            # Assign
-            p_true = torch.tensor(
-                [2 / 14, 4 / 14, 5 / 14, 2 / 14, 1 / 14],
-                device=device,
-                dtype=dtype,
-            )
-            node = Node(p_true)
-
-            # Act
-            sut = NodeSampler(node)
-
-            samples = torch.empty(self.num_samples, device=device, dtype=torch.int32)
-            for i in range(self.num_samples):
-                samples[i] = sut.sample([])
-
-            # Assert
-            p_true_cpu = p_true.cpu().double()
-            p_true_cpu /= p_true_cpu.sum()
-            expected = p_true_cpu * self.num_samples
-
-            samples, _ = samples.sort()
-            actual = [len(list(group)) for _, group in groupby(samples)]
-
-            _, p = stats.chisquare(actual, expected)
-
-            self.assertGreater(p, self.alpha)
-
-
-class TestTorchNodeSamplersCpu(TorchNodeSamplerTestBase.TestTorchNodeSampler):
+class TestTorchNodeSampler(TestCase):
+    @abstractmethod
     def get_torch_settings(self) -> TorchSettings:
-        return TorchSettings(torch.device("cpu"), torch.double)
-
-
-class TestTorchNodeSamplersCuda(TorchNodeSamplerTestBase.TestTorchNodeSampler):
-    def get_torch_settings(self) -> TorchSettings:
-        return TorchSettings(torch.device("cuda"), torch.double)
+        return TorchSettings()
 
     def setUp(self):
-        if not torch.cuda.is_available():
-            self.skipTest("Cuda not available")
+        self.num_samples = 10000
+        self.alpha = 0.001
 
-        super(TestTorchNodeSamplersCuda, self).setUp()
+    def test_node_sampler(self):
+        device = self.get_torch_settings().device
+        dtype = self.get_torch_settings().dtype
 
+        # Assign
+        p_true = torch.tensor(
+            [2 / 14, 4 / 14, 5 / 14, 2 / 14, 1 / 14],
+            device=device,
+            dtype=dtype,
+        )
+        node = Node(p_true)
 
-class TestTorchNodeSamplersMps(TorchNodeSamplerTestBase.TestTorchNodeSampler):
-    def get_torch_settings(self) -> TorchSettings:
-        return TorchSettings(torch.device("mps"), torch.float32)
+        # Act
+        sut = NodeSampler(node)
 
-    def setUp(self):
-        if not torch.has_mps:
-            self.skipTest("Mps not available")
+        samples = torch.empty(self.num_samples, device=device, dtype=torch.int32)
+        for i in range(self.num_samples):
+            samples[i] = sut.sample([])
 
-        super(TestTorchNodeSamplersMps, self).setUp()
+        # Assert
+        p_true_cpu = p_true.cpu().double()
+        p_true_cpu /= p_true_cpu.sum()
+        expected = p_true_cpu * self.num_samples
+
+        samples, _ = samples.sort()
+        actual = [len(list(group)) for _, group in groupby(samples)]
+
+        _, p = stats.chisquare(actual, expected)
+
+        self.assertGreater(p, self.alpha)
